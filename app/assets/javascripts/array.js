62c019562ddde8a9d6308592304f47c8ef4d3a70
Jack Desert <jackdesert@gmail.com>
2017-03-22 08:02:13 -0500

    Generating desaturates

--- app/assets/javascripts/array.js
+++ app/assets/javascripts/array.js
@@ -1,25 +1,62 @@
 var buildColorArray = function(parentDiv){
 
-  var generateDivs = function(parentDiv, colorArray){
+  var div = function(color, klass){
+    console.log('div')
+    return "<div class='" + klass + "' style='background-color: " + color + "'></div>"
+  }
+
+//  var generateDivs = function(parentDiv, colorArray){
+//    var coloredBox = function(color){
+//      return "<div class='colored-box' style='background-color: " + item + "'></div>"
+//    }
+//
+//    var $parentDiv = $(parentDiv)
+//    colorArray.map(function(item){
+//      $parentDiv.append(coloredBox(item))
+//    })
+//
+//  }
+
+  var generateGrid = function(parentDiv, colorArray){
     var $parentDiv = $(parentDiv)
-    colorArray.map(function(item){
-      $parentDiv.append("<div class='colored-box' style='background-color: " + item + "'></div>")
+    colorArray.map(function(color){
+      var desaturates = generateDesaturates(color)
+      var lastDiv
+      $parentDiv.append(div('black', 'main-color'))
+      $lastDiv = $parentDiv.children().last()
+      desaturates.map(function(desaturate){
+        $lastDiv.append(div(desaturate, 'desaturate colored-box'))
+      })
     })
 
   }
 
+
   var generateColorArray = function(){
     var bezInterpolator = chroma.bezier(['white', 'yellow', 'red', 'black'])
-    var numericalArray = [0, 0.33, 0.66, 1]
-    var colorArray = numericalArray.map(function(item){
-      return bezInterpolator(item).hex()
-    })
-    return colorArray
+    return bezInterpolator.scale().colors(4)
   }
 
   var colorArray = generateColorArray()
 
-  generateDivs(parentDiv, colorArray)
+  var generateDesaturates = function(color){
+    var targetLuminance = chroma(color).luminance()
+    var desaturate = chroma('grey').luminance(targetLuminance)
+    var bezInterpolator = chroma.bezier([color, desaturate])
+    return bezInterpolator.scale().colors(5)
+  }
+
+  var generateTints = function(color){
+
+  }
+
+  var generateShades = function(color){
+
+  }
+
+
+
+  generateGrid(parentDiv, colorArray)
 
 
 }
